4ffc585e72b5923ee88af4dfcbabb5570bba1731
Mario Valencia <c_mvalencia@heq.corp.he>
2020-04-02 18:10:59 -0600

    .

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,14 @@
-import { asyncScheduler } from 'rxjs';
+import { range, fromEvent } from 'rxjs';
+import { map } from 'rxjs/operators';
 
+// range(1,5).
+// pipe(
+//     map<number, string>(val => (val * 25).toString())
+// ).
+// subscribe(console.log);
 
-
-const saludar = () => console.log('hola mundo');
-const saludar2 = nombre => {
-  console.log('hola mundo', nombre)
-};
-
-// asyncScheduler.schedule(saludar, 2000);
-// asyncScheduler.schedule(saludar2, 2000, 'Fernando');
-const subs = asyncScheduler.schedule(function(state){
-  console.log(state);
-
-   this.schedule(state, 2000);
-}, 2000, {
-  asdf: 'adsfa',
-  zxcv: 'zxcv'
-});
-
-// setTimeout( () => {
-//   subs.unsubscribe();
-// }, 6000);
-
-asyncScheduler.schedule(function(state){
-  subs.unsubscribe();
-  console.log('unsubscribed');
-}, 6000)
+fromEvent<KeyboardEvent>(document, 'keyup')
+.pipe(
+    map(keyEvent => keyEvent.code)
+)
+.subscribe(console.log);
